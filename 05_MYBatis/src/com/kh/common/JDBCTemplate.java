52251ba5d7e116e2618a584dc7668cce2148cde3
이름 <>
2025-09-08 15:53:39 +0900

    first commit

--- 05_MYBatis/src/com/kh/common/JDBCTemplate.java
+++ 05_MYBatis/src/com/kh/common/JDBCTemplate.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Statement; 
 import java.util.Properties;
 
 public class JDBCTemplate {
